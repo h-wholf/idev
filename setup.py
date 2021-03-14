de97b8f5a7bd4a725425c3261025c2c3d382028c
h-wholf <kemalmoto.e5@gmail.com>
2021-03-13 21:41:39 -0600

    setup 1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'idev',
+  packages = ['idev'], # this must be the same as the name above
+  version = '1.0',
+  description = 'FRAMEWORK DE LA API DE TERMUX PARA USAR EN PYTHON ',
+  author = 'JOAQUIN OSORIO VAZQUEZ h-wholf',
+  author_email = 'kemalmoto.e5@gmail.com',
+  url = 'https://github.com/h-wholf/idev', # use the URL to the github repo
+  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
