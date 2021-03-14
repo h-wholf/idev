25ae52a9a12c76bb69d6c44da1fb2fde83a36d16
h-wholf <kemalmoto.e5@gmail.com>
2021-03-14 00:06:24 -0600

    prueba 2

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+
+from setuptools import setup, find_packages
+
 setup(
-  name = 'idev',
-  packages = ['idev'], # this must be the same as the name above
-  version = '1.0',
-  description = 'FRAMEWORK DE LA API DE TERMUX PARA USAR EN PYTHON ',
-  author = 'JOAQUIN OSORIO VAZQUEZ h-wholf',
-  author_email = 'kemalmoto.e5@gmail.com',
-  url = 'https://github.com/h-wholf/idev', # use the URL to the github repo
-  download_url = 'https://github.com/h-wholf/idev/tarball/0.2',
-  keywords = ['TERMUX API', 'FRAMEWORK', 'IDEV'],
-  classifiers = [],
-)
+    name = 'idev',
+    packages = ['idev'],   
+    include_package_data=True,    # muy importante para que se incluyan archivos sin extension .py
+    version = '0.1',
+    description = 'Termux framework para la api de termux en pytjon',
+    author='joaquin osorio vazquez',
+    author_email="kemalmoto.e5@gmail.com",
+    license="GPLv3",
+    url="https://github.com/h-whokf/idev",
+    classifiers = ["Programming Language :: Python :: 3",\
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",\
+        "Development Status :: 0.1 bera", "Intended Audience :: Developers", \
+        "Operating System :: OS Independent"],
+    )
